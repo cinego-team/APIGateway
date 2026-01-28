7145e45b81a30d821438022186d447958a0ace41
Gianella Rocha <gianellarochavendivengo123@gmail.com>
2026-01-28 12:13:46 -0300

    cambio para poder usar los 2 fronted al mismo tiempo

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.enableCors({ origin: 'http://localhost:4200', credentials: true });
+    await app.enableCors({ origin: ['http://localhost:4200', 'http://localhost:4300'], credentials: true });
     await app.listen(3000);
 }
 bootstrap();
