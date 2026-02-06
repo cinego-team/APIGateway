c5d1780fb9ea129b828fa43a64fe3c8a5934e64a
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-06 19:43:45 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.enableCors({ origin: ['http://localhost:4200', 'http://localhost:4300'], credentials: true });
+    await app.enableCors({
+        origin: [
+            'https://fronted-usuario.vercel.app',
+            'https://fronted_empleado.vercel.app'
+        ],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true
+    });
     await app.listen(3000);
 }
 bootstrap();
