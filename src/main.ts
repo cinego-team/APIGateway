03828a23d9e2b7e552167c1dc66bc04c21517516
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-06 21:05:51 -0300

    testfix

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,12 @@ async function bootstrap() {
         allowedheaders: 'Content-Type, Authorization',
         credentials: true
     });
+    app.use((req, res, next) => {
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(204);
+        }
+        next();
+    });
     await app.listen(3000);
 }
 bootstrap();
