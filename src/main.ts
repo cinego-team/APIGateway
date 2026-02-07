8d091c246f2d6a6dbab187fe627ed7a18b831251
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-06 21:00:27 -0300

    testfix

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.enableCors({
-        origin: [
-            'https://fronted-usuario.vercel.app',
-            'https://fronted_empleado.vercel.app'
-        ],
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-        credentials: true
-    });
+    // app.enableCors({
+    //     origin: [
+    //         'https://fronted-usuario.vercel.app',
+    //         'https://fronted_empleado.vercel.app'
+    //     ],
+    //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    //     credentials: true
+    // });
+    app.enableCors({ origin: true, credentials: true });
     await app.listen(3000);
 }
 bootstrap();
