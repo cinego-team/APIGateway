2b7cd8f3b0691ecc15e442925509f4d4ac0c16c2
Gianella Rocha <gianellarochavendivengo123@gmail.com>
2026-02-25 15:55:01 -0300

    quitado completo de logs

--- src/services/captcha_service/captcha-service.ts
+++ src/services/captcha_service/captcha-service.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 @Injectable()
 export class CaptchaService {
     async validateCaptcha(token: string) {
-        const secret = "6LcICREsAAAAAA2CasYcSYZ9gdRdf45R8goPWEwp";
+        const secret = '6LcICREsAAAAAA2CasYcSYZ9gdRdf45R8goPWEwp';
         const url = `https://www.google.com/recaptcha/api/siteverify`;
 
         const response = await axios.post(
@@ -18,10 +18,8 @@ export class CaptchaService {
         const data = response.data;
 
         if (!data.success) {
-            console.log('Captcha inválido:', data);
             throw new BadRequestException('Captcha inválido');
         }
-        console.log('Captcha válido:', data);
         return true;
     }
 }
